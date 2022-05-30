dab90c403470ad8e5f6d1b89ad92f6e748794b40
blackmed007 <92275188+blackmed007@users.noreply.github.com>
2022-05-30 15:30:48 +0200

    Add files via upload

---
+++ prim3/prim3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inp;
+            do
+            {
+                Console.Write("Input a number to be the limit your Sieve of Erastothenes , and i will print all prime numbers : ");
+                inp=Convert.ToInt32(Console.ReadLine());
+            } while (inp < 2);
+
+            bool[] primenums = new bool[inp];
+            for (int i = 0; i < inp; i++)
+            {
+                primenums[i] = true;
+            }
+
+            for (int i = 2; i < inp; i++)
+            {
+                if (primenums[i - 1])
+                {
+                    for (int t = i * i; t < inp; t += i)
+                    {
+                        primenums[t - 1] = false;
+                    }
+                }
+            }
+            Console.WriteLine("\nthe result is : \n");
+            for (int i = 2; i < inp; i++)
+                if (primenums[i - 1])
+                { Console.Write($" {i} "); }
+
+
+
+
+
+        }
+    }
+}
